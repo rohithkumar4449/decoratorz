f94a6ad2b7137e58e8126ced6879f46dfd90f1e1
rohith kumar reddy <rohithkumar4449@gmail.com>
2024-12-19 20:15:03 +0530

    jdf

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,6 +1,6 @@
  // Ensure the carousel starts sliding automatically
  var myCarousel = document.querySelector('#videoCarousel')
  var carousel = new bootstrap.Carousel(myCarousel, {
-     interval: 6000,
+     interval: 4000,
      pause: false // Ensures carousel continues sliding even on hover
  });
