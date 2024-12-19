a42ad6afd75ebd45848353b463759a82a533af3d
rohith kumar reddy <rohithkumar4449@gmail.com>
2024-12-19 13:14:30 +0530

    jj

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,4 +1,6 @@
-var carousel = new bootstrap.Carousel(document.querySelector('#videoCarousel'), {
-    interval: 6000, // Slides will change every 6 seconds
-    ride: 'carousel'
-});
+ // Ensure the carousel starts sliding automatically
+ var myCarousel = document.querySelector('#videoCarousel')
+ var carousel = new bootstrap.Carousel(myCarousel, {
+     interval: 6000,
+     pause: false // Ensures carousel continues sliding even on hover
+ });
