7885e09288d2f0121b772b29a6e064fedc20beb3
rohith kumar reddy <rohithkumar4449@gmail.com>
2024-12-24 19:35:41 +0530

    ggg

---
+++ frontend/assets/js/testimonials.js
@@ -0,0 +1,14 @@
+// const testimonialsContainer = document.querySelector('.testimonials-container');
+// const cards = document.querySelectorAll('.card');
+// let currentIndex = 0;
+// const totalCards = cards.length;
+
+// function slideTestimonials() {
+//     const cardWidth = cards[0].clientWidth;
+//     const offset = -currentIndex * cardWidth;
+//     testimonialsContainer.style.transform = `translateX(${offset}px)`;
+
+//     currentIndex = (currentIndex + 1) % totalCards;
+// }
+
+// setInterval(slideTestimonials, 3000);
